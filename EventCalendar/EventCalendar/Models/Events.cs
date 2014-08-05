ed17440e4e4a84170a15a20f0d5c43208f5f9b08
Madan Mohan Venkat Baggu <madanvenkat535@gmail.com>
2014-08-05 15:11:38 +0530

    eventcalendar
    eventcalendar

---
+++ EventCalendar/EventCalendar/Models/Events.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventCalendar.Models
+{
+    public class Events
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string date { get; set; }
+        public string start { get; set; }
+        public string end { get; set; }
+        public string url { get; set; }
+
+        public bool allDay { get; set; }
+    }
+}
